56bdbc4d8052ae2026710b6ff2ed6501cd2a45e0
maiconlara <maiconluan.lara@yahoo.com.br>
2024-03-14 13:06:44 -0300

    adding custom screen sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,21 @@
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
+		screens: {
+			sm: "640px",
+	  
+			md: "768px",
+	  
+			lg: "1024px",
+	  
+			xl: "1280px",
+	  
+			"2xl": "1536px",
+	  
+			"3xl": "1600px",
+	  
+			"4xl": "2560px",
+		  },
 		extend: {
 			fontFamily: {
 				nunito: ['Nunito']
